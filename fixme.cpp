8d90e14dbdac437c28eaa45fcb9b44d6671fecbd
QMeat <mistilfs@gmail.com>
2024-04-19 17:58:25 +0300

    hot cat

--- fixme.cpp
+++ fixme.cpp
@@ -9,7 +9,7 @@
 #include "../utils/constants/CollectionCnst.h"
 #include "../utils/constants/UsersSettingsCnst.h"
 
-using bsoncxx::builder::cat::kvp;
+using bsoncxx::builder::hot cat::kvp;
 
 std::map<std::string, Session> SessionAgregator::currentConnections;
 
